e2aed272779b48f2f868ddfdad51971391bbe0d1
vedaminishukla26 <vedaminishukla26@gmail.com>
2025-05-19 20:40:07 +0530

    deployment trial

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// if (process.env.NODE_ENV !== "production") {
+//     require('dotenv').config();
+// }
+
 const express = require("express");
 const path = require('path');
 const mongoose = require('mongoose');
@@ -6,9 +10,13 @@ const session = require('express-session');
 const flash =  require('connect-flash');
 const ExpressError =  require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const usersRoutes = require('./routes/users');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -37,9 +45,17 @@ const sessionConfig = {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}
+};
+
 app.use(session(sessionConfig));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next) => {
    res.locals.success = req.flash('success');
@@ -47,8 +63,9 @@ app.use((req,res,next) => {
    next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', usersRoutes);
+app.use('/campgrounds', campgroundsRoutes);
+app.use('/campgrounds/:id/reviews', reviewsRoutes);
 
 app.get('/', (req,res) => {
     res.render('home')
@@ -64,9 +81,11 @@ app.use((err,req,res,next) => {
     res.status(statusCode).render('error',{err});
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on port ${port}`);
+});
+
 
 
 
